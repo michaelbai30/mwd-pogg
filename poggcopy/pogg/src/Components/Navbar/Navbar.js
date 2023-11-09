702fd470f444c2127c129aba196cf4b2f8c612a5
Michael Bai <89441879+michaelbai30@users.noreply.github.com>
2023-11-08 19:13:25 -0500

    Update Navbar.js

--- poggcopy/pogg/src/Components/Navbar/Navbar.js
+++ poggcopy/pogg/src/Components/Navbar/Navbar.js
@@ -1,21 +1,68 @@
 import { Link } from "react-router-dom";
+import Parse from "parse";
+import { logOutUser } from "../Auth/AuthService.js";
+import { useState } from "react";
 
-const Navbar = () => (
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Main</Link>
-        </li>
-        <li>
-          <Link to="/matchhistory">Match History</Link>
-        </li>
-        <li>
-          <Link to="/leaderboards">Leaderboards</Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
-);
+const Navbar = () => {
+  // Check if user is authenticated
+  const isAuthenticated = Parse.User.current();
+
+  //note: the navbar does not update until reload of the page
+
+  return (
+    <div>
+      <nav>
+        <ul>
+          {isAuthenticated ? (
+            <>
+              <li>
+                <Link to="/">Main</Link>
+              </li>
+              <li>
+                <Link to="/matchhistory">Match History</Link>
+              </li>
+              <li>
+                <Link to="/leaderboards">Leaderboards</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to="/">
+                  {" "}
+                  <button onClick={logOutUser}>Logout</button>{" "}
+                </Link>
+              </li>
+              {/* <li>
+                {" "}
+                <button onClick={logOutUser}>Logout</button>{" "}
+              </li> */}
+            </>
+          ) : (
+            <>
+              {/* <li>
+                <Link to="./auth/login">Login</Link>
+              </li>
+              <li>
+                <Link to="./auth/register">Register</Link>
+              </li> */}
+              <li>
+                <Link to="/auth">Login/Register</Link>
+              </li>
+
+              <li>
+                <Link to="/">Main</Link>
+              </li>
+              <li>
+                <Link to="/matchhistory">Match History</Link>
+              </li>
+              <li>
+                <Link to="/leaderboards">Leaderboards</Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
