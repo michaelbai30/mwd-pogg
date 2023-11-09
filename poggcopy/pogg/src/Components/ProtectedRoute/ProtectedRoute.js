fe9feb99bb9b92386eca2e7fbd7c075baf88bfb0
Michael Bai <89441879+michaelbai30@users.noreply.github.com>
2023-11-08 19:13:46 -0500

    Update ProtectedRoute.js

--- poggcopy/pogg/src/Components/ProtectedRoute/ProtectedRoute.js
+++ poggcopy/pogg/src/Components/ProtectedRoute/ProtectedRoute.js
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { checkUser } from "../Auth/AuthService";
 
-//starter code from class
-//TODO: add proper protected route stuff
-
+//mostly from class
+//used to limit access if not logged in
 // You can pass props using the spread operator to throw them on an object if there are too many to break out
 const ProtectedRoute = ({ element: Component, ...rest }) => {
   console.log("element: ", Component);
+  // return (
+  //   <div>
+  //     <p>1</p>
+  //   </div>
+  // );
   const navigate = useNavigate();
   const goBackHandler = () => {
-    navigate("/auth");
+    console.log("hello");
+    navigate("/");
   };
+
+  useEffect(() => {
+    if (!checkUser()) {
+      navigate("/auth");
+    }
+  });
   if (checkUser()) {
+    // if (0) {
     return <Component />;
   } else {
     return (
-
-      <div>
-        <p>Unauthorized!</p> <button onClick={goBackHandler}>Go Back.</button>
-      </div>
+      //  This will simply redirect upon loading; means they are not logged in
+      <p>redirect</p>
+      //shouldn't reach here, should be redirected by useeffect
+      // <button onClick={goBackHandler}>
+      //   direct to home
+      //   {/* <p>should direct back to the home page.</p> */}
+      // </button>
     );
   }
 };
 
 export default ProtectedRoute;
-
