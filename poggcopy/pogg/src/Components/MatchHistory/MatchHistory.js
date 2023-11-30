7d7251fdd6cb5ec4d6aace08a822f520a48183e7
mspad4 <mspadafo@nd.edu>
2023-11-30 15:47:56 -0500

    Started Match History advanced implementation

--- poggcopy/pogg/src/Components/MatchHistory/MatchHistory.js
+++ poggcopy/pogg/src/Components/MatchHistory/MatchHistory.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { getAllSumms, getByName } from "../../Services/SummonerService.js";
+import {getSummonerByName} from "../../Services/RiotService.js";
 import {
   getMatches,
   populateMatches,
-  getMatchesBySumm
+  getMatchesBySumm,
+  singleMatch
 } from "../../Services/MatchService.js";
 import Minimatch from "./Minimatch.js";
 
@@ -15,11 +17,24 @@ const MatchHistory = () => {
 
   //this function is kinda janky for now
   const handleClick = () => {
-    getByName(summonerName)
-      .then((data) => setActiveSumm(data))
-      .then(() => {
-        getMatchesBySumm(activeSumm).then((lis) => setMatches(lis));
-      });
+        getSummonerByName(summonerName).then( (nm) => {
+          var puuid = nm.puuid;
+          console.log(puuid);
+          var sMatches = getMatches(puuid).then( (sMatches) => {
+            var matchesInfo = Promise.all(sMatches.map(singleMatch));
+            
+            return matchesInfo
+          }).then ((minfo) => {
+            console.log(minfo);
+            setMatches(minfo);
+          })
+        })
+    // getByName(summonerName)
+    //   .then((data) => setActiveSumm(data))
+    //   .then(() => {
+    //     getSummonerByName()
+    //     // getMatchesBySumm(activeSumm).then((lis) => setMatches(lis));
+    //   });
 
     // .then(getMatches(activeSumm.attributes.puuid))
     // .then((dat) => populateMatches(dat, activeSumm));
@@ -48,21 +63,25 @@ const MatchHistory = () => {
         Note: if stats don't appear, try waiting a bit and clicking again. Use
         Protossian as the user for an example
       </p>
-      <select
+      <label for="summoner">Summoner to Lookup:</label>
+
+      <input
+        type="text"
         name="summs"
         value={summonerName}
         onChange={(e) => {
           setSummonerName(e.target.value);
         }}
       >
-        {summs.map(function (sname) {
+        {/* {summs.map(function (sname) {
           return (
             <option key={sname.attributes.summonername}>
               {sname.attributes.summonername}
             </option>
           );
-        })}
-      </select>
+        })} */}
+
+      </input>
       <button onClick={handleClick}>Get Match History</button>
       {matches.length > 0 && <Minimatch m={matches} />}
     </div>
--- poggcopy/pogg/src/Components/MatchHistory/Minimatch.js
+++ poggcopy/pogg/src/Components/MatchHistory/Minimatch.js
@@ -1,14 +1,52 @@
 import React from "react";
 //displays match information very basic
 const Minimatch = ({ m }) => {
-  console.log("m:", m);
+  //console.log("m:", m);
+  //console.log(m[0].info["gameid"])
   return (
     <div>
       <h1>Match Type and Won</h1>
+
+
       <ul>
         {m.map((mat) => (
-          <li key={Math.random()}>
-            {mat.attributes.gamemode} | {mat.attributes.win.toString()}
+          <li key={mat.info.gameId}>
+            {mat.info.gameMode} 
+            {console.log(mat.info.participants)}
+            <button>
+    <a type="button" href={mat.metadata.matchId}> Advanced Stats</a>
+   </button> 
+            <br />
+            <ul>
+
+          {/* <h4>Team 1 </h4> */}
+
+          {mat.info.teams[0].win ? <h4>Team 1 - Win</h4> : <h4>Team 1 - Loss</h4>}
+            {mat.info.participants.slice(mat.info.participants.length / 2).map((p) => {
+            
+              return <li key={p.summonerName}>
+                <img src= {"https://ddragon.leagueoflegends.com/cdn/13.23.1/img/profileicon/" + p.profileIcon + ".png"} height="35" width="35" alt="Profile Icon"/>
+                {p.summonerName}
+                
+                <img src= {"https://ddragon.leagueoflegends.com/cdn/13.23.1/img/champion/" + p.championName + ".png"} height="35" width="35" alt={p.championName}/>
+                
+                {p.kills} / {p.deaths} / {p.assists}
+                </li>
+            })}
+            <br/>
+            {mat.info.teams[1].win ? <h4>Team 2 - Win</h4> : <h4>Team 2 - Loss</h4>}
+
+            {mat.info.participants.slice(0,mat.info.participants.length / 2).map((p) => {
+            
+            return <li key={p.summonerName}>
+                              <img src= {"https://ddragon.leagueoflegends.com/cdn/13.23.1/img/profileicon/" + p.profileIcon + ".png"} height="35" width="35" alt="Profile Icon"/>
+{p.summonerName}
+<img src= {"https://ddragon.leagueoflegends.com/cdn/13.23.1/img/champion/" + p.championName + ".png"} height="35" width="35" alt={p.championName}/>
+{p.kills} / {p.deaths} / {p.assists}
+</li>
+          })}
+          <br/>
+            </ul>
           </li>
         ))}
       </ul>
